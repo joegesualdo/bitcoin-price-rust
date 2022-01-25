0ceaf3e5459c7124238e2368d723d5440f4032c3
Joe Gesualdo <joegesualdo@gmail.com>
2022-01-25 10:03:20 -0500

    Add functionality to get average of all exchange prices

--- examples/example.rs
+++ examples/example.rs
@@ -2,8 +2,11 @@ use bitcoin_price;
 fn main() {
     let coinbase_price = bitcoin_price::get_coinbase_price();
     let coinbase_price_string = coinbase_price.to_string();
-    println!("{}", coinbase_price_string);
+    println!("coinbase: {}", coinbase_price_string);
     let kraken_price = bitcoin_price::get_kraken_price();
     let kraken_price_string = kraken_price.to_string();
-    println!("{}", kraken_price_string);
+    println!("kraken: {}", kraken_price_string);
+    let average_exchange_price = bitcoin_price::get_average_exachange_spot_price();
+    let average_exchange_price_string = average_exchange_price.to_string();
+    println!("average_exchange_price: {}", average_exchange_price_string);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,8 @@ pub fn get_coinbase_price() -> f32 {
 pub fn get_kraken_price() -> f32 {
     return kraken::get_spot_price();
 }
+pub fn get_average_exachange_spot_price() -> f32 {
+    let coinbase_price: f32 = coinbase::get_spot_price();
+    let kraken_price: f32 = kraken::get_spot_price();
+    return (coinbase_price + kraken_price)/2.0
+}
