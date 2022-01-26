6e3ca8a000a75e964e1e8f77c4620b1d3c474907
Joe Gesualdo <joegesualdo@gmail.com>
2022-01-26 13:58:38 -0500

    Clean code

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@ mod kraken;
 pub fn get_coinbase_price() -> f32 {
     return coinbase::get_spot_price();
 }
+
 pub fn get_kraken_price() -> f32 {
     return kraken::get_spot_price();
 }
+
 pub fn get_average_exachange_spot_price() -> f32 {
     let coinbase_price: f32 = coinbase::get_spot_price();
     let kraken_price: f32 = kraken::get_spot_price();
