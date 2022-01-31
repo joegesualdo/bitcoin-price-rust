b38df68a96bfd07168b15ad128d04babfb2d1284
Joe Gesualdo <joegesualdo@gmail.com>
2022-01-31 13:37:38 -0500

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -12,5 +12,6 @@ pub fn get_kraken_price() -> f32 {
 pub fn get_average_exachange_spot_price() -> f32 {
     let coinbase_price: f32 = coinbase::get_spot_price();
     let kraken_price: f32 = kraken::get_spot_price();
-    return (coinbase_price + kraken_price)/2.0
+    let average_price: f32 = (coinbase_price + kraken_price)/2.0;
+    return  average_price;
 }
